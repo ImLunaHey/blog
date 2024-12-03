661a02a42eeb0123d5fc0754b4dcce8868ef4c8e
luna <luna@wvvw.me>
2024-12-03 22:28:35 +1030

    fix: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
